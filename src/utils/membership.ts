88b6a707f6184942fc89fdf34251ff8203229fda
오승찬 <oseungchan@oseungchan-ui-MacBookAir.local>
2024-07-08 00:27:33 +0900

    [fix]:이메일, 닉네임 중복성 검사 수정 완료

--- src/utils/membership.ts
+++ src/utils/membership.ts
@@ -1,5 +1,6 @@
 import * as Yup from "yup";
 import { auth, db } from "../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import {
   collection,
   doc,
@@ -8,7 +9,6 @@ import {
   setDoc,
   where,
 } from "firebase/firestore";
-import { createUserWithEmailAndPassword } from "firebase/auth";
 
 //// 회원가입
 interface userDataProps {
@@ -17,7 +17,6 @@ interface userDataProps {
   password: string;
   address: string;
 }
-type PartialUserDataProps = Pick<userDataProps, "email" | "nickname">;
 //계정 생성
 export const addNewUser = async ({
   email,
@@ -41,27 +40,31 @@ export const addNewUser = async ({
     throw error;
   }
 };
-export const duplicateCheck = async ({
-  nickname,
-  email,
-}: PartialUserDataProps) => {
-  const nicknameQuery = query(
-    collection(db, "users"),
-    where("displayName", "==", nickname)
-  );
+export const emailCheck = async (email: string) => {
   const emailQuery = query(
     collection(db, "users"),
     where("email", "==", email)
   );
-  const nicknameSnapshot = await getDocs(nicknameQuery);
   const emailSnapshot = await getDocs(emailQuery);
-  return { nickname: nicknameSnapshot.empty, email: emailSnapshot.empty };
+  return emailSnapshot.empty;
+};
+export const nicknameCheck = async (nickname: string) => {
+  const nicknameQuery = query(
+    collection(db, "users"),
+    where("displayName", "==", nickname)
+  );
+  const nicknameSnapshot = await getDocs(nicknameQuery);
+  return nicknameSnapshot.empty;
 };
 // 유효성 검사
 export const joinMembershipSchema = Yup.object({
   email: Yup.string()
     .required("이메일을 입력해 주세요.")
-    .email("올바르지 않은 형식의 이메일 입니다."),
+    .email("올바르지 않은 형식의 이메일 입니다.")
+    .test("checkEmail", "이미 사용중인 이메일 입니다.", async (value) => {
+      const response = await emailCheck(value);
+      return response;
+    }),
   password: Yup.string()
     .required("비밀번호를 입력해 주세요.")
     .min(8, "비밀번호는 8자리에서 16자리 이내여야 합니다.")
@@ -73,7 +76,11 @@ export const joinMembershipSchema = Yup.object({
   nickname: Yup.string()
     .required("닉네임을 입력해주세요")
     .max(12, "닉네임은 최대 12자리까지 입니다")
-    .matches(/^\S*$/, "공란없이 입력해야합니다"),
+    .matches(/^\S*$/, "공란없이 입력해야합니다")
+    .test("checkNickname", "이미 사용중인 닉네임입니다.", async (value) => {
+      const response = await nicknameCheck(value);
+      return response;
+    }),
   confirm_password: Yup.string()
     .required("비밀번호 확인이 필요합니다.")
     .oneOf([Yup.ref("password")], "비밀번호가 일치하지 않습니다"),
