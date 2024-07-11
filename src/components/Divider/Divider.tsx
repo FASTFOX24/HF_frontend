bc43cbd2c3e9b10abd22149a049253d4449ae84e
오승찬 <oseungchan@oseungchan-ui-MacBookAir.local>
2024-07-11 23:52:27 +0900

    [feat]:디바이더 1차 커밋

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,8 +1,34 @@
 import React from "react";
-import * as S from "./styled";
+import styled from "styled-components";
+import theme from "../../styles/theme";
 
 const Divider = () => {
-  return <S.Container>Divider</S.Container>;
+  return (
+    <Container>
+      <DividerBody />
+      <DividerText></DividerText>
+      <DividerBody />
+    </Container>
+  );
 };
 
 export default Divider;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 312px;
+  margin: 28px 0px;
+`;
+const DividerBody = styled.hr`
+  margin: 0px;
+  width: 40%;
+  border: 0px;
+  border-top: 1px solid ${theme.color.gray.G300};
+  border-radius: 12px;
+`;
+const DividerText = styled.span`
+  ${theme.text.body_2}
+  color: ${theme.color.gray.G300};
+`;
